324019d1ddfd673734b5dc67d2b39ef0fe070573
MarcosHBlanco <124410355+MarcosHBlanco@users.noreply.github.com>
2025-03-25 11:32:50 -0700

    styling changes to showing pokemons

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -11,22 +11,32 @@ export default function PokemonList({ pokemons, addToPokedex }) {
 					}}
 				>
 					<h2>
-						{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+						<strong>
+							{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+						</strong>
 					</h2>
-					<img src={pokemon.sprites.front_default} alt={pokemon.name} />
-					<p>
-						<strong>Height:</strong> {pokemon.height}
-					</p>
-					<p>
-						<strong>Weight:</strong> {pokemon.weight}
-					</p>
-					<p>
-						<strong>Type:</strong> {pokemon.types[0].type.name}
-					</p>
-					<p>
-						<strong>Hp:</strong>{" "}
-						{pokemon.stats.find((s) => s.stat.name === "hp").base_stat}
-					</p>
+					<div className="flex place-items-center">
+						<div className="w-20 ">
+							<img src={pokemon.sprites.front_default} alt={pokemon.name} />
+						</div>
+						<p>
+							<strong>Height:</strong> {pokemon.height}
+							{" | "}
+						</p>
+						<p>
+							<strong>Weight:</strong> {pokemon.weight}
+							{" | "}
+						</p>
+						<p>
+							<strong>Type:</strong> {pokemon.types[0].type.name}
+							{" | "}
+						</p>
+						<p>
+							<strong>Hp:</strong>{" "}
+							{pokemon.stats.find((s) => s.stat.name === "hp").base_stat}
+							{" | "}
+						</p>
+					</div>
 					<p>
 						<strong>Moves:</strong>{" "}
 						{pokemon.moves.slice(0, 15).map((move, index) => (
